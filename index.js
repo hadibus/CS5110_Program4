587f8a8893774924698e2b82e13100c995b31d9c
Christopher <chris.jenkins20102012@gmail.com>
2018-04-17 14:26:12 -0600

    pretty done

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 "use strict";
 const commandLineUsage = require('command-line-usage');
-const use = require('./requires/usage');
 const usage = commandLineUsage(require('./requires/usage.js'));
 
 const commandLineArgs = require("command-line-args");
@@ -18,19 +17,31 @@ if (!options.bids || !options['ad-clicks'])
   process.exit();
 }
 
-console.log("do work...");
-
-const bids = options.bids.split(',');
+const bids = options.bids.split(',')
+  .sort(function(a, b){return b-a});
 for (var i = 0; i < bids.length; i++)
 {
   bids[i] = Number(bids[i]);
 }
-const ad_clicks = options['ad-clicks'].split(',');
+
+const ad_clicks = options['ad-clicks'].split(',')
+  .sort(function(a, b){return b-a});
 for (var i = 0; i < ad_clicks.length; i++)
 {
   ad_clicks[i] = Number(ad_clicks[i]);
 }
 
-console.log('bids', bids);
-console.log('ad_clicks', ad_clicks);
+console.log('bid quantities are:', bids);
+console.log('ad clicks are:     ', ad_clicks);
 
+for (var i = 0; i < bids.length; i++)
+{
+  //calculate what each agent will pay.
+  var total = 0;
+  for (var j = i; j < ad_clicks.length && j + 1 < bids.length; j++) 
+  {
+    total += bids[j+1] * 
+      (ad_clicks[j] - (j + 1 == ad_clicks.length? 0 : ad_clicks[j + 1]));
+  }
+  console.log("A" + i, "pays", total);
+}
